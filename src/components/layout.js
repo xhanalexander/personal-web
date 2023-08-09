b99cadbebd5943df5e437610b023e91eecce78d4
xhanalexander <xhanalexander@gmail.com>
2023-08-10 03:11:22 +0700

    prevent

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,29 @@
 import React from 'react'
 import Navbar from './navbar'
 import Footer from './footer'
+import { useEffect } from 'react';
 
 export default function Layout({ children }) {
+  useEffect(() => {
+    const preventDefault = (event) => {
+      event.preventDefault();
+    };
+
+    document.body.ondragstart = preventDefault;
+    document.body.oncontextmenu = preventDefault;
+    document.body.onselectstart = preventDefault;
+    document.body.onkeydown = preventDefault;
+    document.body.onmousedown = preventDefault;
+
+    return () => {
+      document.body.ondragstart = null;
+      document.body.oncontextmenu = null;
+      document.body.onselectstart = null;
+      document.body.onkeydown = null;
+      document.body.onmousedown = null;
+    };
+  }, []);
+
   return (
     <article className='max-w-2xl mx-auto'>
       <Navbar />
