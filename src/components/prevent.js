cb293302a765791279e6627b3a173a168a83b46d
xhanalexander <xhanalexander@gmail.com>
2023-08-12 03:06:45 +0700

    prevent function

---
+++ src/components/prevent.js
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+
+const usePreventDefault = () => {
+  useEffect(() => {
+    const preventDefault = (event) => {
+      event.preventDefault();
+    };
+
+    document.body.ondragstart = preventDefault;
+    document.body.oncontextmenu = preventDefault;
+    document.body.onselectstart = preventDefault;
+    document.body.onkeydown = preventDefault;
+    document.body.onmousedown = preventDefault;
+
+    return () => {
+      document.body.ondragstart = null;
+      document.body.oncontextmenu = null;
+      document.body.onselectstart = null;
+      document.body.onkeydown = null;
+      document.body.onmousedown = null;
+    };
+  }, []);
+};
+
+export default usePreventDefault;
