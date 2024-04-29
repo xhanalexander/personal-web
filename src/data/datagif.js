fa8a5a655d8d9e7d1fb276d541917af43bf4fd19
unknown <xhanalexander>
2024-04-29 21:59:34 +0700

    minor fix

--- src/data/datagif.js
+++ src/data/datagif.js
@@ -4,19 +4,19 @@ export const gifImages = [
   {
     id: 1,
     href: "https://xhanalexander.github.io/preview1/",
-    src: "https://media0.giphy.com/media/uC3VXdAH68r6MGF6zB/giphy.gif",
+    src: "https://zj1oyfw7hcdtrci0.public.blob.vercel-storage.com/1-Ui0tSAjencunZT6utzIQ6kfwFbEl4c.webp",
     alt: "Touch n Slide - 1",
   },
   {
     id: 2,
     href: "https://xhanalexander.github.io/preview3/",
-    src: "https://media1.giphy.com/media/lL6avFKPlFo28FMLXs/giphy.gif",
+    src: "https://zj1oyfw7hcdtrci0.public.blob.vercel-storage.com/2-8Meau9QwEEf4RXjbp8n0zRiApKpz7z.webp",
     alt: "Touch n Slide - 2",
   },
   {
     id: 3,
     href: "https://xhanalexander.github.io/preview2/",
-    src: "https://media3.giphy.com/media/YVDmw3CgUkWZmujcnz/giphy.gif",
+    src: "https://zj1oyfw7hcdtrci0.public.blob.vercel-storage.com/3-v8pBtL3ZDZ1DOWYXvcbQcQ1Yrr8xnu.webp",
     alt: "Tilt Type Text",
   },
   /* {
@@ -28,31 +28,31 @@ export const gifImages = [
   {
     id: 5,
     href: "https://xhanalexander.github.io/preview5/",
-    src: "https://media1.giphy.com/media/d2SOF16uMs0VPpOkwU/giphy.gif",
+    src: "https://zj1oyfw7hcdtrci0.public.blob.vercel-storage.com/5-GTTFJVRfvp27NM58hSmlnLokLe0yfo.webp",
     alt: "Dancing LSD Gyroscope",
   },
   {
     id: 6,
     href: "https://xhanalexander.github.io/preview6/",
-    src: "https://media0.giphy.com/media/emxdNqNmxNC5Rpu07o/giphy.gif",
+    src: "https://zj1oyfw7hcdtrci0.public.blob.vercel-storage.com/6-GJTEfxr6muqVubEoLMgwzv9oXTHrVq.webp",
     alt: "Differential Growth - 1",
   },
   {
     id: 7,
     href: "https://xhanalexander.github.io/preview7/",
-    src: "https://media3.giphy.com/media/49S1SHiPaf7Xxc15vx/giphy.gif",
+    src: "https://zj1oyfw7hcdtrci0.public.blob.vercel-storage.com/7-1M4tNl9RnnzH8ipFiseDvzfVSTZrxB.webp",
     alt: "Dancing Spinning LSD Rotate",
   },
   {
     id: 8,
     href: "https://xhanalexander.github.io/preview8/",
-    src: "https://media0.giphy.com/media/cnxYIIphn2hsNpebAx/giphy.gif",
+    src: "https://zj1oyfw7hcdtrci0.public.blob.vercel-storage.com/8-nQCsTfodXXMLbsrR4f8JFFp6y1LmyG.webp",
     alt: "Diffusion Scale",
   },
   {
     id: 9,
     href: "https://xhanalexander.github.io/preview9/",
-    src: "https://media2.giphy.com/media/ts4jn2OU7B6UnSKNfZ/giphy.gif",
+    src: "https://zj1oyfw7hcdtrci0.public.blob.vercel-storage.com/9-t9axkq1g4OFArc0is31tu0f2oCMOG5.webp",
     alt: "Stretch Text",
   },
 ];
